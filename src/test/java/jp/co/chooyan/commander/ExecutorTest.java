26ca17f33bb07f430ae976f573408a324cac5ba2
chooyan_eng <chooyan.engineer@gmail.com>
2016-10-12 21:36:34 +0900

    [add] basic test sources

---
+++ src/test/java/jp/co/chooyan/commander/ExecutorTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.chooyan.commander;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author tsuyoshi-chujo
+ */
+public class ExecutorTest {
+    @Test
+    public void testMain() {
+        Executor.main(null);
+    }
+}
