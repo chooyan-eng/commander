7be32e9f384c45ef28f9ba371c88036e34dd6fee
chooyan_eng <chooyan.engineer@gmail.com>
2016-10-27 16:33:05 +0900

    [add] implement main

--- src/main/java/jp/co/chooyan/commander/Executor.java
+++ src/main/java/jp/co/chooyan/commander/Executor.java
@@ -5,12 +5,28 @@
  */
 package jp.co.chooyan.commander;
 
+import java.nio.file.Paths;
+import jp.co.chooyan.commander.core.ClassInstanciator;
+import jp.co.chooyan.commander.core.analyze.Analyzer;
+import jp.co.chooyan.commander.core.config.Config;
+import jp.co.chooyan.commander.core.output.Outputter;
+import jp.co.chooyan.commander.core.parse.Parser;
+
 /**
  *
  * @author tsuyoshi-chujo
  */
 class Executor {
     public static void main(String args[]) {
+        Config config = Config.parse(Paths.get("src", "main", "resources", "config.yml").toString());
+
+        Parser parser = (Parser) ClassInstanciator.instanciate(config.getParserName());
+        Analyzer analyzer = (Analyzer) ClassInstanciator.instanciate(config.getAnalyzerName());
+        Outputter outputter = (Outputter) ClassInstanciator.instanciate(config.getOutputterName());
+        
+        Object parsedObject = parser.parse(Paths.get("src", "test", "resources", "test_command.txt").toString());
+        Object analyzedObject = analyzer.analyze(parsedObject);
+        outputter.output(analyzedObject);
         
     }
 }
